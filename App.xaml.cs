077f1114f38fb89af90ae748590da3bf693bc690
duvaniesmart <122755644+duvaniesmart@users.noreply.github.com>
2023-04-19 08:06:54 -0500

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+using WaterReminderMaui.Services;
+using WaterReminderMaui.Views;
+
+namespace WaterReminderMaui
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
